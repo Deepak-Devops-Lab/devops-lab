4f5805b02b02e0e91c5a87a9fac542303719bca4
Deepak kumar <dk.kumar201306@gmail.com>
2026-07-28 09:45:56 +0000

    added demo files

--- git-lab/demo.py
+++ git-lab/demo.py
@@ -1,7 +1,9 @@
-def my_function():
+"""Simple module demonstrating a function that returns a constant value."""
 
-    a = 5
-    return a
+
+def my_function():
+    """Return the integer value 5."""
+    return 5
 
 
 my_function()
